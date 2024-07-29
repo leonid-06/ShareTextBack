a273038682f057f5be2e5d74f52e334a87098aed
leonid-06 <harinlenya1488@gmail.com>
2024-07-29 23:21:43 +0300

    init

---
+++ src/main/java/com/leonid/sharetext/ShareTextBackApplication.java
@@ -0,0 +1,13 @@
+package com.leonid.sharetext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareTextBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareTextBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leonid/sharetext/ShareTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonid.sharetext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareTextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
